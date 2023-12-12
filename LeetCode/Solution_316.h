0e6a91d38cb9a4929456dc765aa02bfc98cdb203
HCH96 <sjrnfl0412@kookmin.ac.kr>
2023-12-12 21:30:50 +0900

    LeetCode 316 Solved

--- LeetCode/Solution_316.h
+++ LeetCode/Solution_316.h
@@ -5,6 +5,44 @@ class Solution {
 public:
     string removeDuplicateLetters(string s) {
 
+        string ans;
 
+        unordered_map<char, int> m;
+        unordered_map<char, int> check;
+        
+        stack<char> st;
+
+        for (int i = 0; i < s.size(); ++i)
+        {
+            ++m[s[i]];
+        }
+
+        for (int i = 0; i < s.size(); ++i)
+        {
+            char Cur = s[i];
+            --m[Cur];
+
+            if (check[Cur] == 1)
+                continue;
+
+            while (!st.empty() && st.top() > Cur && m[st.top()] > 0)
+            {
+                char Prev = st.top();
+                st.pop();
+                --check[Prev];
+            }
+
+            st.push(Cur);
+            ++check[Cur];
+        }
+
+
+        while (!st.empty())
+        {
+            ans.insert(ans.begin(), st.top());
+            st.pop();
+        }
+
+        return ans;
     }
 };
