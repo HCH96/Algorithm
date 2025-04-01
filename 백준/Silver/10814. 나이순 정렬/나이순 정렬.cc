c0fa6daddb4583b8035ff23f188179b6c490889c
HCH96 <sjrnfl0412@kookmin.ac.kr>
2025-04-01 12:59:03 +0900

    [Silver V] Title: 나이순 정렬, Time: 84 ms, Memory: 9732 KB -BaekjoonHub

---
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+bool compare(const pair<int, string> &a, const pair<int, string> &b)
+{
+    return a.first < b.first;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<pair<int, string>> v;
+    for (int i = 0; i < n; i++)
+    {
+        int age;
+        string name;
+        cin >> age >> name;
+        v.push_back({age, name});
+    }
+
+    stable_sort(v.begin(), v.end(), compare);
+
+    for(int i=0; i<n; i++){
+        cout<< v[i].first  << " " << v[i].second << "\n";
+    }
+}
