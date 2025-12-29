8adea62f2c6e3043e875891d5ea66fc3c4d53028
HCH96 <sjrnfl0412@kookmin.ac.kr>
2025-12-30 02:03:28 +0900

    [Silver V] Title: 아무래도이문제는A번난이도인것같다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1402. 아무래도이문제는A번난이도인것같다/아무래도이문제는A번난이도인것같다.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#include <sstream>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int T;
+    cin >> T;
+
+    while (T--)
+    {
+        int a, b;
+        cin >> a >> b;
+
+        cout << "yes\n";
+
+
+    }
+    
+    return 0;
+}
