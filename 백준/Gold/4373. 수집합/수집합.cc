68146e6899cd2ad9f9d4f539fbd2589a4d64375b
HCH96 <sjrnfl0412@kookmin.ac.kr>
2025-12-31 03:00:09 +0900

    [Gold I] Title: 수집합, Time: 112 ms, Memory: 14816 KB -BaekjoonHub

--- 백준/Gold/4373. 수집합/수집합.cc
+++ 백준/Gold/4373. 수집합/수집합.cc
@@ -36,12 +36,15 @@ int main() {
             }
         } 
 
-        for (int i = N - 1; i >= 1; --i)
+        for (int i = N - 1; i >= 0; --i)
         {
             long long d = v[i];
 
-            for (int j = i - 1; j >= 0; --j)
+            for (int j = 0; j < N ; ++j)
             {
+                if (i == j)
+                    continue;
+
                 long long Target = d - v[j];
 
                 if (um.find(Target) != um.end())
