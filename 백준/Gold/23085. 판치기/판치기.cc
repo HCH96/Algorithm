37b0fc0fe52af72c2aed94b0df1168e16c1786dd
HCH96 <sjrnfl0412@kookmin.ac.kr>
2026-01-02 02:25:57 +0900

    [Gold IV] Title: 판치기, Time: 12 ms, Memory: 3324 KB -BaekjoonHub

---
+++ 백준/Gold/23085. 판치기/판치기.cc
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#include <sstream>
+#include <unordered_set>
+
+using namespace std;
+
+int N, K;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    cin >> N >> K;
+
+    string s;
+    cin >> s;
+
+    int Size = s.size();
+    int head = 0;
+    int tail = 0;
+    for (int i = 0; i < s.size(); ++i)
+    {
+        if (s[i] == 'H')
+            ++head;
+        else
+            ++tail;
+    }
+
+    vector<vector<bool>> visited(Size+1, vector<bool>(Size+1, false));
+
+    visited[head][tail] = true;
+    queue<vector<int>> q;
+    q.push({ head,tail,0 });
+    while (!q.empty())
+    {
+        int CurHead = q.front()[0];
+        int CurTail = q.front()[1];
+        int CurCount = q.front()[2];
+        q.pop();
+
+        if (CurHead == 0 && CurTail == Size)
+        {
+            cout << CurCount;
+            return 0;
+        }
+
+        for (int i = 0; i <= K; ++i)
+        {
+            int HeadFlip = i;
+            int TailFlip = K - i;
+            
+            if (HeadFlip > CurHead || TailFlip > CurTail)
+                continue;
+
+            int NextHead = CurHead + TailFlip - HeadFlip;
+            int NextTail = CurTail + HeadFlip - TailFlip;
+
+            if (NextHead<0 || NextHead >Size || NextTail<0 || NextTail >Size)
+                continue;
+
+            if (visited[NextHead][NextTail] == false)
+            {
+                visited[NextHead][NextTail] = true;
+                q.push({ NextHead ,NextTail ,CurCount + 1 });
+            }
+        }
+
+
+    }
+
+    cout << -1;
+
+    return 0;
+}
